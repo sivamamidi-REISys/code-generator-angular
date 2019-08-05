bb5a669f4f7cf6fee7eb5ac0b8c1f34bdec94053
sivamamidi-REISys <siva.mamidi@reisystems.com>
2019-08-05 10:47:35 -0400

    fixed debugger issue

--- configuration.js
+++ configuration.js
@@ -26,6 +26,38 @@ exports.configuration = {
         }
       ]
      
+    },
+       "Announcement": {
+      "actionType": "crud",
+      "pluralWord" : "Announcements",
+      "uniqueId" : "announcementId",
+      "properties": [
+        {
+          "name": "announcementId",
+          "display": "Id",
+          "type": "integer",
+          "required" : true
+        },
+        {
+          "name": "title",
+          "display": "Announcement Title",
+          "type": "string",
+          "required" : true
+        },
+        {
+          "name": "description",
+          "display": "Description",
+          "type": "string",
+          "required" : true
+        },
+        {
+          "name": "isActive",
+          "display": "isActive",
+          "type": "boolean",
+          "required" : false
+        }
+      ]
+     
     }
   }
 }
