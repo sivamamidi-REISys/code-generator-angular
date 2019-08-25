8580d5ddabbe56a0193d0e89b37d43731e342cfc
sivamamidi-REISys <siva.mamidi@reisystems.com>
2019-08-25 15:49:03 -0400

    fixed team page bdd

--- templatesBDD/pages/definitionName/singularWordPage.java
+++ templatesBDD/pages/definitionName/singularWordPage.java
@@ -3,7 +3,7 @@ import gov.gsa.comet.cucumber.PageObject;
 import gov.gsa.comet.helpers.Utilities;
 import org.openqa.selenium.By;
 
-public class Create{{singularWord}}Page extends PageObject {
+public class {{singularWord}}Page extends PageObject {
 
     private static final By CREATE_BUTTON = By.id("createButton");
     private static final By EDIT_BUTTON = By.id("editButton");
